0417987139d08a75ab41db621de219fb6719db28
sumedh-gaikwad <gaikwadsumedh019@gmail.com>
2024-02-15 20:22:11 +0530

    Routers added(basic routing added)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,20 +2,23 @@
 import './App.css';
 import Navigation from './customer/components/Navigation/Navigation';
 import HomePage from './pages/HomePage/HomePage';
-import Footer from './customer/components/Footer/Footer';
-import Product from './customer/components/Product/Product';
+import Footer from './customer/components/footer/Footer';
+import Product from './customer/components/Product/product/Product';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 export default function App() {
   return (
     <div className='bg-yellow-100'>
 
-      
-          <Navigation/>
-          <div>
-             {/* <HomePage/> */}
-             <Product/> 
-          </div>
-          <Footer/>
+
+      <Navigation />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage/>}/>
+        </Routes>
+      </BrowserRouter>
+
+      <Footer />
     </div>
 
   )
